df1cc8a67e047419e28e85b3480a718dc6ab4610
randolph <mayson.dy@gmail.com>
2025-04-26 02:48:46 -0400

    all code changes

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,18 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: "Access denied. No token provided." });
+  }
+
+  jwt.verify(token, "fingerprint_customer", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid token." });
+    }
+    req.user = decoded;
+    next();
+  });
 });
  
 const PORT =5000;
